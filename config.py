216107a22de30a7d686017fbd6f5a249d5622723
trym1d <dmitry213@mail.com>
2025-07-15 22:51:19 +0300

    final_changes_3

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://c2a41abf-c9e7-489f-95bb-814e6355f46d.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://04255776-6e5b-41b0-8451-505bd22ae743.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_ORDER_PATH = "/api/v1/orders/"
 GET_ORDER_VIA_TRACK = "/v1/orders/track?t="
