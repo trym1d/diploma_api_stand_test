2a0a9844bbd044507514f1e7fc2ad58df911bcd0
trym1d <dmitry213@mail.com>
2025-07-16 23:28:50 +0300

    final_changes_5

--- order_test.py
+++ order_test.py
@@ -1,7 +1,16 @@
 # Дмитрий Горлов 31 когорта, финальный проект. Инженер по тестированию плюс.
 
+import data
 import sender_stand_request
 
+
 def test_positive_order_status():
-    response = sender_stand_request.get_order()
+    # Запрос на создание заказа
+    sender_stand_request.create_order(
+        data.order_body)
+    # Сохраняем трек-номер в переменную
+    track = sender_stand_request.get_track_order()
+    # Запрос заказа по сохранённой переменной
+    response = sender_stand_request.get_order(track)
+    # Проверка кода ответа
     assert response.status_code == 200
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,15 +3,26 @@ import config
 import data
 
 # Создание заказа
+
+
 def create_order(body):
-    return requests.post(config.URL_SERVICE + config.CREATE_ORDER_PATH, json=body, headers=data.headers)
-response = create_order(data.order_body)
+    return requests.post(
+        config.URL_SERVICE +
+        config.CREATE_ORDER_PATH,
+        json=body,
+        headers=data.headers)
 
 # Получаем номер трека созданного заказа
+
+
 def get_track_order():
-    return response.json()["track"]
-track = get_track_order()
+    return create_order(data.order_body).json()["track"]
 
 # Получаем заказ по трекномеру
-def get_order():
-    return requests.get(config.URL_SERVICE + config.GET_ORDER_VIA_TRACK + str(track))
+
+
+def get_order(track):
+    return requests.get(
+        config.URL_SERVICE +
+        config.GET_ORDER_VIA_TRACK +
+        str(track))
